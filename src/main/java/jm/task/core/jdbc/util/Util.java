84d9791bd178d3a4573ae4d160d68ff0b689738b
annalana <devil-villow@inbox.ru>
2022-05-23 10:23:13 +0300

    JavaPP 1.1.3 - version 1

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,27 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    public static Connection getConnection() {
+        return getConnection("jdbc:mysql://localhost:3306", "root", "Eve02041991");
+    }
+    public static Connection getConnection(String user, String password) {
+        return getConnection("jdbc:mysql://localhost:3306", user, password);
+    }
+    public static Connection getConnection(String adress, String user, String password) {
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(adress, user, password);
+            System.out.println("Log in base successfully");
+        } catch (Exception e) {
+            System.out.println("Error while log in base");
+            e.printStackTrace();
+        }
+        return connection;
+    }
 }
+
